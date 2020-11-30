7b7407afeed7acf843dec80846c56f6b5c87333a
rajgurjar97 <rajgurjarji97@gmail.com>
2020-11-30 15:41:53 +0530

    first commit

---
+++ src/main/java/com/lumenore/spring/docker/jenkins/api/DockerJenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.lumenore.spring.docker.jenkins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumenore/spring/docker/jenkins/api/DockerJenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumenore.spring.docker.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
